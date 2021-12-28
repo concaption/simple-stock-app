9513b8cac7c8ed36c403666f325d53bb97526d05
Muhammad Usama Navid <concaption@gmail.com>
2021-12-28 19:09:38 +0500

    Update app.py

--- stock_price/app.py
+++ stock_price/app.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+import datetime as dt
 
 
 st.title("Stock Price App")
@@ -9,12 +10,26 @@ st.write("""
          """)
 
 
-ticker_symbol = st.selectbox("Which stoock would you like to keep track of?", ('AAPL','MSFT','GOOG','FB','AMZN','TSLA','OTHER'))
+ticker_symbol = st.sidebar.selectbox("Which stoock would you like to keep track of?", ('AAPL','MSFT','GOOG','FB','AMZN','TSLA','OTHER'))
 if ticker_symbol=='OTHER':
-    ticker_symbol=st.text_input("Enter the stock symbol","For example: AAPL")
-st.write("You selected: ", ticker_symbol)
+    ticker_symbol=st.sidebar.text_input("Enter the stock symbol","For example: AAPL")
+st.sidebar.write("You selected: ", ticker_symbol)
 ticker_data = yf.Ticker(ticker_symbol)
-ticker_dataframe = ticker_data.history(period="1d", start="2020-01-01", end="2020-04-01")
+
+
+today = dt.date.today()
+year_ago = today - dt.timedelta(days=365)
+
+start_date = st.sidebar.date_input("Start Date", value=year_ago)
+end_date = st.sidebar.date_input("End Date", value=today)
+ticker_dataframe = ticker_data.history(period="1d", start=start_date, end=end_date)
+
+if end_date > start_date:
+    st.sidebar.success('Start date: %s\n\nEnd date: %s' % (start_date, end_date))
+else:
+    st.sidebar.error('Error: End date must fall after start date.')
+
+
 st.line_chart(ticker_dataframe.Close)
 st.line_chart(ticker_dataframe.Open)
 st.line_chart(ticker_dataframe.High)
